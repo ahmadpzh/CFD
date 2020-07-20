e4109afd480a96097cdcaceec1d858e22bfd2eca
ahmad7238 <53912114+ahmad7238@users.noreply.github.com>
2020-07-20 14:06:38 +0430

    major correction

--- discre_2.py
+++ discre_2.py
@@ -9,8 +9,8 @@ time1 = time.time()
 # # Setting
 l_x = 600  # length
 l_y = 400  # width
-n_x = 30  # grid in x-axis
-n_y = 20  # grid in y-axis
+n_x = 60  # grid in x-axis
+n_y = 40  # grid in y-axis
 n_x_max = n_x + 1
 n_y_max = n_y + 1
 x0 = 0
@@ -18,7 +18,7 @@ y0 = 0
 dx = (l_x / n_x)
 dy = (l_y / n_y)
 # variables (Acceleration Part)
-x = np.reshape(np.zeros(n_y_max * n_x_max), [n_y_max, n_x_max])
+x = np.reshape(np.zeros(n_y * n_x), [n_y, n_x])
 y = np.copy(x)
 gamma = np.copy(x)
 gamma_e = np.copy(x)
@@ -26,8 +26,8 @@ gamma_w = np.copy(x)
 gamma_n = np.copy(x)
 gamma_s = np.copy(x)
 
-for i in range(n_y_max):
-    for j in range(n_x_max):
+for i in range(n_y_max - 1):
+    for j in range(n_x_max - 1):
         x[i][j] = float(j) * dx
         y[i][j] = float(i) * dy
         gamma[i][j] = 2000
@@ -40,9 +40,9 @@ iteration = 0
 
 # # Initial Condition
 # 50[m] for every cells except top boundary
-h_old = np.reshape([float(d_a) for i in np.zeros(n_y_max * n_x_max)], [n_y_max, n_x_max])
+h_old = np.reshape([float(d_a) for i in np.zeros(n_y * n_x)], [n_y, n_x])
 # creating zero matrix for process acceleration
-h_new = np.reshape([float(d_a) for i in np.zeros(n_y_max * n_x_max)], [n_y_max, n_x_max])
+h_new = np.reshape([float(d_a) for i in np.zeros(n_y * n_x)], [n_y, n_x])
 
 # # Boundary Condition
 # top boundary
@@ -61,13 +61,11 @@ q = 0.01  # newmann boundary [m/day]
 
 
 # # Processing
-tdiff = std_error + std_error
-# while error > std_error:
-while iteration < 200:
+while error > std_error:
     iteration += 1
 
-    for i in range(1, n_y_max - 1):
-        for j in range(1, n_x_max - 1):
+    for i in range(1, n_y - 1):
+        for j in range(1, n_x - 1):
             gamma_e = (gamma[i][j + 1] + gamma[i][j]) / 2
             gamma_w = (gamma[i][j - 1] + gamma[i][j]) / 2
             gamma_n = (gamma[i - 1][j] + gamma[i][j]) / 2
@@ -76,10 +74,10 @@ while iteration < 200:
             dx_w = x[i][j] - x[i][j - 1]
             dy_n = y[i][j] - y[i - 1][j]
             dy_s = y[i + 1][j] - y[i][j]
-            area_e = y[i + 1][j + 1] - y[i][j + 1]
-            area_w = y[i + 1][j] - y[i][j]
-            area_n = x[i][j + 1] - x[i][j]
-            area_s = x[i + 1][j + 1] - x[i + 1][j]
+            area_e = dy_n * d_a
+            area_w = dy_n * d_a
+            area_n = dx_e * d_a
+            area_s = dx_w * d_a
 
             a_known_e = gamma_e * area_e / dx_e
             a_known_w = gamma_w * area_w / dx_w
@@ -88,41 +86,50 @@ while iteration < 200:
             s_p = 0.
             s_u = 0.
 
-            # main domain
-            a_p = a_known_e + a_known_w + a_known_n + a_known_s
-            h_new[i][j] = ((1 / a_p) * (a_known_w * h_old[i][j - 1] + a_known_e * h_old[i][j + 1] +
-                                        a_known_n * h_old[i - 1][j] + a_known_s * h_old[i + 1][j] + s_u))
+        if i == 1:
+            # top boundary
+            h_old[0] = 100
 
-    # left_boundary (constant_flow)
-    j = 0
-    for i in range(1, n_y_max):
-        if i == n_y_max - 1:
-            h_new[i][j] = (1 / (a_known_e + a_known_n + a_known_s)) * ((2 * a_known_e) * h_old[i][j + 1] +
-                                                                       a_known_n * h_old[i - 1][0] + q * area_e)
-        else:
-            h_new[i][j] = (1 / (a_known_e + a_known_n + a_known_s)) * ((2 * a_known_e) * h_old[i][j + 1] +
-                                                                       a_known_n * h_old[i - 1][0] + a_known_s *
-                                                                       h_old[i + 1][0] + q * area_n)
+        # left_boundary (constant_flow)
+        if j == 0:
+            a_known_w = 0
 
-    error = np.linalg.norm(h_new, 1)
-    # error2 = h_new - h_old
+        # # # left boundary (no flow)
+        # j = 0
+        # for i in range(1, n_y_max - 1):
+        #     h_new[i][j] = (1 / (a_known_e + a_known_n + a_known_s)) * (a_known_e * h_old[i][j + 1] +
+        #                                                                a_known_n * h_old[i - 1][j] + a_known_s *
+        #                                                                h_old[i + 1][j])
+
+        # right boundary (no flow)
+        if j == n_y - 1:
+            a_known_e = 0
+        # bottom boundary (no flow)
+        if i == n_y - 1:
+            a_known_s = 0
+
+        a_p = a_known_e + a_known_w + a_known_n + a_known_s
+
+        h_new[i][j] = ((1 / a_p) * (a_known_w * h_old[i][j - 1] + a_known_e * h_old[i][j + 1] +
+                                    a_known_n * h_old[i - 1][j] + a_known_s * h_old[i + 1][j] + s_u))
+
+    error = np.linalg.norm(h_new - h_old, 2)
     print('\nL2Norm = %0.5f' % error)
-    # print(error2)
 
     print('iteration = ', iteration)
 
-    # print('L2Norm = ', L2Norm)
+    # plt.contourf(h_new)
+    # plt.gca().invert_yaxis()
+    # plt.axis('off')
+    # plt.grid()
+    # plt.colorbar().ax.set_ylabel('[m]')
+    # plt.pause(0.001)
+    # plt.show(block=False)
+    # plt.clf()
 
-    plt.contourf(h_new)
-    plt.gca().invert_yaxis()
-    plt.axis('off')
-    plt.grid()
-    plt.colorbar().ax.set_ylabel('[m]')
-    plt.pause(0.001)
-    plt.show(block=False)
-    plt.clf()
-
-    h_old = h_new
+    for i in range(n_y_max - 1):
+        for j in range(n_x_max - 1):
+            h_old[i][j] = h_new[i][j]
 
 print('L2Norm = ', np.linalg.norm(h_new))
 print('iteration = ', iteration)
